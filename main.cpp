4a88486c5e4de74db10284a593d78d0ab3d3aef6
Alejandr0112 <erickalejandro0112@gmail.com>
2025-02-14 14:01:45 -0600

    Ejercicio 2 xd

--- main.cpp
+++ main.cpp
@@ -17,3 +17,28 @@ int main() {
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+long long factorial(int n) {
+    long long resultado = 1;
+    for (int i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+int main2() {
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    if (numero < 0) {
+        cout << "El factorial no está definido para números negativos." << endl;
+    } else {
+        cout << "El factorial de " << numero << " es " << factorial(numero) << endl;
+    }
+
+    return 0;
+}
